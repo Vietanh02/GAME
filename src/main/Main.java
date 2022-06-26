3b050d185672008cb5dc3d3c5c49f6145a1866ff
vanh <anhsptk123@gmail.com>
2022-06-26 21:12:24 +0700

    IntelliJ

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,6 +2,7 @@ package main;
 import javax.swing.JFrame;
 public class Main {
 	public static void main(String[] args) {
+		//commit thuwr
 		// tạo 1 cửa sổ game
 		JFrame window = new JFrame();
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
