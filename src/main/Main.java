d98de3268120128d0d79607d363d75ca882c8be6
vanh <anhsptk123@gmail.com>
2022-06-26 21:14:14 +0700

    check1

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,7 +2,7 @@ package main;
 import javax.swing.JFrame;
 public class Main {
 	public static void main(String[] args) {
-		//commit thuwr
+		//commit dc ko vay
 		// tạo 1 cửa sổ game
 		JFrame window = new JFrame();
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
