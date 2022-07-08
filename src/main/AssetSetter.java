de9a16b9730e6544cf4bbfb58eef559eafe3818b
nnk14102002 <78344784+nnk14102002@users.noreply.github.com>
2022-07-08 16:09:41 +0700

    Revert "sửa map (#30)"
    This reverts commit c777ccca9467755d2850b8807559282efca5f84d.

--- src/main/AssetSetter.java
+++ src/main/AssetSetter.java
@@ -2,7 +2,7 @@ package main;
 
 //import entity.NPC_LittleRed;
 import entity.NPC_OldMan;
-import object.*;
+import monster.MON_GreenSlime;
 
 public class AssetSetter {
     GamePanel gp;
@@ -11,26 +11,20 @@ public class AssetSetter {
     }
 
     public void setObject(){
-        gp.obj[0] = new OBJ_Key(gp);
-        gp.obj[0].worldX = 12*gp.tileSize;
-        gp.obj[0].worldY = 6*gp.tileSize;
 
-        gp.obj[1] = new OBJ_Door(gp);
-        gp.obj[1].worldX = 13*gp.tileSize;
-        gp.obj[1].worldY = 13*gp.tileSize;
-
-        gp.obj[2] = new OBJ_Chest(gp);
-        gp.obj[2].worldX = 6* gp.tileSize;
-        gp.obj[2].worldY = 5*gp.tileSize;
-
-        gp.obj[3] = new OBJ_Port(gp);
-        gp.obj[3].worldX = 18* gp.tileSize;
-        gp.obj[3].worldY = 18*gp.tileSize;
     }
     public void setNPC(){
-        gp.NPC[0] = new NPC_OldMan(this.gp);
+        gp.NPC[0] = new NPC_OldMan(gp);
         gp.NPC[0].worldX = 5*gp.tileSize;
         gp.NPC[0].worldY = 10*gp.tileSize;
+
+
+    }
+    public void setMonster(){
+
+        gp.monster[0] = new MON_GreenSlime(gp);
+        gp.monster[0].worldX = 5*gp.tileSize;
+        gp.monster[0].worldY = 5*gp.tileSize;
 
     }
 }
--- src/main/EventHandler.java
+++ src/main/EventHandler.java
@@ -1,35 +1,39 @@
 package main;
 
 import java.awt.*;
-import java.awt.image.BufferedImage;
 
 
 public class EventHandler {
     GamePanel gp;
-    EventRect[][] eventRect;
+    EventRect eventRect[][];
     int previousEventX, previousEventY;
     boolean canTouchEvent = true;
 //
  
     public EventHandler(GamePanel gp) {
         this.gp = gp;
+
         eventRect = new EventRect[gp.maxWorldCol][gp.maxWorldRow];
+
         int col = 0;
         int row = 0;
         while (col < gp.maxWorldCol && row < gp.maxWorldRow) {
             eventRect[col][row] = new EventRect();
-            eventRect[col][row].x = 22;
-            eventRect[col][row].y = 22;
-            eventRect[col][row].width = 4;
-            eventRect[col][row].height = 4;
+            eventRect[col][row].x = 23;
+            eventRect[col][row].y = 23;
+            eventRect[col][row].width = 2;
+            eventRect[col][row].height = 2;
             eventRect[col][row].eventRectDefaultX = eventRect[col][row].x;
             eventRect[col][row].eventRectDefaultY = eventRect[col][row].y;
+
             col++;
             if (col == gp.maxWorldCol) {
                 col = 0;
                 row++;
             }
         }
+
+
     }
     public void checkEvent(){
         int xDistance = Math.abs(gp.player.worldX - previousEventX);
@@ -38,26 +42,30 @@ public class EventHandler {
         if(distance > gp.tileSize){
             canTouchEvent = true;
         }
-        if(canTouchEvent){
-            if (hit(3,3, "any")) {damagePit(gp.dialogueState);}
-            if (hit(19,19,2,1,"any")) {telemap(gp.dialogueState);}
-            if(hit(10,10,"any")){healingPool(gp.dialogueState);}
+        if(canTouchEvent == true){
+            if (hit(3,3, "any") == true ) {damagePit(3,3,gp.dialogueState);}
+//            if (hit(5,8,"any")== true) {telemap(5,8,gp.dialogueState);}
+//            if(hit(10,10,"up") == true){healingPool(gp.dialogueState);}
         }
     }
 
     public boolean hit(int col, int row, String reqDirection){
+
         boolean hit = false;
+
         gp.player.solidArea.x = gp.player.worldX + gp.player.solidArea.x;
         gp.player.solidArea.y = gp.player.worldY + gp.player.solidArea.y;
         eventRect[col][row].x = col*gp.tileSize + eventRect[col][row].x;
         eventRect[col][row].y = row*gp.tileSize + eventRect[col][row].y;
-        if(gp.player.solidArea.intersects(eventRect[row][col])&& !eventRect[col][row].eventDone){
+
+        if(gp.player.solidArea.intersects(eventRect[row][col])&&eventRect[col][row].eventDone == false){
             if(gp.player.direction.contentEquals(reqDirection) || reqDirection.contentEquals("any")){
                 hit = true;
                 previousEventX = gp.player.worldX;
                 previousEventY = gp.player.worldY;
             }
         }
+
         gp.player.solidArea.x = gp.player.solidAreaDefaultX;
         gp.player.solidArea.y = gp.player.solidAreaDefaultY;
         eventRect[col][row].x = eventRect[col][row].eventRectDefaultX;
@@ -65,25 +73,8 @@ public class EventHandler {
 
         return hit;
     }
-    public boolean hit(int x,int y,int width, int height, String reqDirection){
-        boolean hit = false;
-        Rectangle rect = new Rectangle(gp.tileSize*x,gp.tileSize*y,gp.tileSize*width,gp.tileSize*height);
-        gp.player.solidArea.x = gp.player.worldX + gp.player.solidArea.x;
-        gp.player.solidArea.y = gp.player.worldY + gp.player.solidArea.y;
-        if(gp.player.solidArea.intersects(rect)){
-            if(gp.player.direction.contentEquals(reqDirection) || reqDirection.contentEquals("any")){
-                hit = true;
-                previousEventX = gp.player.worldX;
-                previousEventY = gp.player.worldY;
-            }
-        }
-        rect.x = eventRect[0][0].eventRectDefaultX;
-        rect.y = eventRect[0][0].eventRectDefaultY;
-        gp.player.solidArea.x = gp.player.solidAreaDefaultX;
-        gp.player.solidArea.y = gp.player.solidAreaDefaultY;
-        return hit;
-    }
-    public void damagePit(int gameState){
+
+    public void damagePit(int col, int row, int gameState){
         gp.gameState = gameState;
         gp.ui.currentDialogue = "Vap co va nga";
         gp.player.life -= 1;
@@ -92,17 +83,16 @@ public class EventHandler {
     }
 
 
-    public void telemap (int gameState){
+    public void telemap (int col, int row, int gameState){
         gp.gameState = gameState ;
-        gp.ui.currentDialogue = "dich chuyen";
-        gp.player.worldX = gp.tileSize * 10;
+        gp.player.worldX = gp.tileSize * 12;
         gp.player.worldY = gp.tileSize * 13;
     }
     public void healingPool(int gameState){
-        if(gp.keyH.enterPressed){
+        if(gp.keyH.enterPressed == true){
             gp.gameState = gameState;
             gp.ui.currentDialogue = "Ban choi da va hoi mau";
-            if(gp.player.life < 6) gp.player.life += 1;
+            gp.player.life += 1;
         }
     }
 }
