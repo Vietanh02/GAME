8e4034bbe27d6f92e9efd6ec9d88435771ded608
phamcuong02 <foreverlovepham2002@gmail.com>
2022-07-10 11:31:12 +0700

    add rock file

---
+++ src/entity/Projectile/Ptile_rock.java
@@ -0,0 +1,44 @@
+package entity.Projectile;
+
+import entity.Entity;
+import main.GamePanel;
+
+public class Ptile_rock extends Projectile {
+        GamePanel gp;
+        public Ptile_rock(GamePanel gp){
+        super(gp);
+        this.gp = gp;
+        name = "Rock";
+        speed = 5;
+        maxLife = 80;
+        life = maxLife;
+        atk = 5;
+        skillManaCost = 1;
+        alive = false;
+        getImage();
+    }
+
+    private void getImage() {
+        String imageDown1 = "/bullet/rock_down_1";
+        String imageUp1 = "/bullet/rock_down_1";
+        String imageLeft1 = "/bullet/rock_down_1";
+        String imageRight1 = "/bullet/rock_down_1";
+
+        for(int i=0;i<2;i++) {
+            up[i]   = setup(String.format(imageUp1,i+1),gp.tileSize, gp.tileSize);
+            down[i] = setup(String.format(imageDown1,i+1), gp.tileSize, gp.tileSize );
+            left[i] = setup(String.format(imageLeft1,i+1),gp.tileSize, gp.tileSize);
+            right[i] = setup(String.format(imageRight1,i+1),gp.tileSize, gp.tileSize);
+        }
+    }
+    public boolean haveResource(Entity user){
+        boolean haveResource = false;
+        if(user.mana >= skillManaCost){
+            haveResource = true;
+        }
+        return haveResource;
+    }
+    public void subtractResource(Entity user){
+        user.mana -= skillManaCost;
+    }
+}
