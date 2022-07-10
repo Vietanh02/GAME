f9d05573c1d3f7ef16db9c17ccf297659323daf8
nnk14102002 <78344784+nnk14102002@users.noreply.github.com>
2022-07-10 09:59:24 +0700

    Revert "Drop item (#40)"
    This reverts commit c57206a652ba926f80e97da98be19de543f0f36c.

--- src/entity/object/OBJ_Heart.java
+++ src/entity/object/OBJ_Heart.java
@@ -6,17 +6,10 @@ import main.GamePanel;
 public class OBJ_Heart extends Entity {
     public OBJ_Heart(GamePanel gp){
         super(gp);
-        this.gp = gp;
         name = "Heart";
-        type = type_pickupOnly;
-        value = 2;
-        stay[0] = setup("/objects/heart_full", gp.tileSize, gp.tileSize);
-        stay[1] = setup("/objects/heart_full", gp.tileSize, gp.tileSize);
-        stay[2] = setup("/objects/heart_full", gp.tileSize, gp.tileSize);
+                stay[0] = setup("/objects/heart_full", gp.tileSize, gp.tileSize);
+                stay[1] = setup("/objects/heart_half", gp.tileSize, gp.tileSize);
+                stay[2] = setup("/objects/heart_blank", gp.tileSize, gp.tileSize);
         collision = false;
-    }
-    public void use(Entity entity){
-        gp.ui.addMessage("Life + " + value);
-        gp.player.life += value;
     }
 }
